2f76e8e75e38b6a6d6da9a0c63d28b0724d71376
Christopher-OOC <olojedechristopher24@gmail.com>
2024-12-15 22:33:17 +0100

    commit

--- src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/controller/KafkaConfig.java
+++ src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/controller/KafkaConfig.java
@@ -8,14 +8,14 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaConfig {
 
-    @Bean
-    NewTopic newTopic() {
-
-        return TopicBuilder
-                .name("test1")
-                .partitions(3)
-                .replicas(1)
-                .config("min.insync.replicas", "1")
-                .build();
-    }
+//    @Bean
+//    NewTopic newTopic() {
+//
+//        return TopicBuilder
+//                .name("test1")
+//                .partitions(3)
+//                .replicas(1)
+//                .config("min.insync.replicas", "1")
+//                .build();
+//    }
 }
